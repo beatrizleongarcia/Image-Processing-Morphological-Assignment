cac110df51e1ec6aaa7d8430990ca010816bb259
Beatriz León García <78873722+beatrizleongarcia@users.noreply.github.com>
2023-05-05 02:02:50 +0200

    Add files via upload

---
+++ functionsMatlab.m
@@ -0,0 +1,35 @@
+% function erosion 
+% originalBW = imread("2.KNEE.png");
+% se = strel('line',25,300);
+% erodedBW = imerode(originalBW,se);
+% figure, imshow(originalBW)
+% figure, imshow(erodedBW), title('erosion:')
+% end
+
+% function grow
+% BW = imread("1.JOINT.png");
+% se = strel('line',25,300);
+% BW2 = imdilate(BW,se);
+% figure, imshow(BW)
+% figure, imshow(BW2), title('grow:')
+% end 
+
+% function hitmiss
+% bw = imread("perroygatofuncionmatlab.png");
+% interval = [0 -1 1 1 1 -1 0 1 0];
+% bw2 = bwhitmiss(bw,interval)
+% figure, imshow(bw)
+% figure, imshow(bw2), title('hit/miss:')
+% end
+
+
+% function skeleton
+% bwhite = imread("perroygatofuncionmatlab.tif");
+% %Icomplement = imcomplement(bwhite);
+% %W = imbinarize(Icomplement);
+% %se = strel('line',25,300);
+% bwhite2 = bwskel(bwhite);
+% %bwhiteout = bwskel(BW);
+% figure, imshow(bwhite)
+% figure, imshow(bwhite2), title('skeleton:')
+% end
